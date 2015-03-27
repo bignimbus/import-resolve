6fc5756fa5e39e9be9f9528cdcbda1300e2df7c5
Jeff Auriemma <Jeff_Auriemma@discovery.com>
2015-03-27 11:31:23 -0500

    alpha candidate

--- index.js
+++ index.js
@@ -1,23 +1,44 @@
 #!/usr/bin/env node
 
-var fs = require('fs'),
-    homedir = './',
-    dist = '';
+var cwd = '',
+    root = '',
+    dist = '',
+    fs = require('fs'),
+    path = require('path'),
+    regex = /@import\s['"](.+?)['"];?/g;
 
 function read (filename) {
-    return fs.readFileSync(homedir + filename, {"encoding": "utf8"});
+    var cwdStr = '',
+        stylesheet = '',
+        dir = filename.split('/');
+    filename = dir.pop();
+    dir = dir.join('/');
+
+    cwd = path.resolve(root, cwd, dir) + '/';
+    console.log(cwd);
+
+    stylesheet =  fs.readFileSync(cwd + filename, {"encoding": "utf8"});
+    stylesheet = stylesheet.replace(regex, function (m, capture) {
+        return m && m.replace(capture, cwd + capture);
+    });
+    return stylesheet;
 }
 
 function resolve (oldFile) {
-    return oldFile.replace(/@import\s['"](.+?)['"];?/g, function (m, capture) {
+    return oldFile.replace(regex, function (m, capture) {
         return read(capture);
     });
 }
 
-module.exports = function styleCompile (path, output) {
-    path = path.split('/');
-    var oldFile, resolved, filename = path.pop();
-    homedir = path.join('/') + '/';
+module.exports = function importResolve (pathToMain, output) {
+    cwd = '';
+    root = '';
+    dist = '';
+    var oldFile, resolved, filename;
+
+    root = (process.cwd() + '/' + pathToMain).split('/');
+    filename = root.pop();
+    root = root.join('/') + '/';
 
     dist = read(filename);
     resolved = false;

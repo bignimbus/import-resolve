2a76c9c0a30dbc792bf0d0cee1a3f343b63382df
Jeff Auriemma <jdauriemma@gmail.com>
2016-08-27 22:52:29 -0400

    Add test specs for #writeToFile

--- tests/lib/import-resolver.spec.js
+++ tests/lib/import-resolver.spec.js
@@ -1,18 +1,52 @@
 'use strict';
 
-var ImportResolver = require('../../lib/import-resolver');
+var ImportResolver = require('../../lib/import-resolver'),
+    fs = require('fs');
 
 describe('ImportResolver', function () {
     var subject;
     describe('constructor', function () {
-        subject = new ImportResolver({
-            "output": "foo",
-            "ext": "bar",
-            "pathToMain": "baz/bing"
+        beforeEach(function () {
+            subject = new ImportResolver({
+                "output": "foo",
+                "ext": "bar",
+                "pathToMain": "baz/bing"
+            });
         });
-        expect(subject.output).toBe('foo');
-        expect(subject.ext).toBe('.bar');
-        expect(subject.root).toEqual(process.cwd().split('/').concat(['baz', 'bing']));
+        afterEach(function () {
+            subject = null;
+        });
+
+        it('should set the output file', function () {
+            expect(subject.output).toBe('foo');
+        });
+        it('should set the extension', function() {
+            expect(subject.ext).toBe('.bar');
+        });
+        it('should set the root path', function () {
+            expect(subject.root).toEqual(process.cwd().split('/').concat(['baz', 'bing']));
+        });
+    });
+
+    describe('#writeToFile', function (done) {
+        beforeEach(function () {
+            subject = new ImportResolver({
+                "output": "foo",
+                "ext": "bar",
+                "pathToMain": "baz/bing"
+            });
+        });
+        afterEach(function () {
+            subject = null;
+        });
+
+        it('should be able to call a callback function', function () {
+            subject.writeToFile('./foo', function () {
+                fs.unlinkSync('./foo');
+                expect(1).toBe(1);
+                done();
+            });
+        });
     });
 });
 
